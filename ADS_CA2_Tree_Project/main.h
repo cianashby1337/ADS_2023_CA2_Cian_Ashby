5143434a1824e5f0002013deab936ac0c3ea11b5
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2023-11-30 01:33:34 +0000

    Pass the ReadSimpleSampleXML test

--- ADS_CA2_Tree_Project/main.h
+++ ADS_CA2_Tree_Project/main.h
@@ -1,6 +1,18 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 
 std::string readFile(std::string filePath) {
-    return 0;
+    std::ifstream inFileStream(filePath);
+    std::string output = "";
+    if (inFileStream.good()) {
+        while (!inFileStream.eof()) {
+            //removing by ascii values under 33 puts input in one line
+            if (inFileStream.peek() > 32) output += inFileStream.peek();
+            inFileStream.get();
+        }
+        inFileStream.close();
+        return output;
+    }
+    else std::cout << "Unable to open file, or file is empty.";
 }
