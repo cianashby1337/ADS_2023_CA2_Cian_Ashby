7793caa13b79bb7107bae09d305c3bcdc1f0734d
Cian Ashby <cianashby1337@gmail.com>
2023-11-30 23:36:44 +0000

    Pass nesting tests for validate method

--- ADS_CA2_Tree_Project/main.h
+++ ADS_CA2_Tree_Project/main.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <stack>
 
 std::string readFile(std::string filePath) {
     std::ifstream inFileStream(filePath);
@@ -17,6 +18,34 @@ std::string readFile(std::string filePath) {
     else std::cout << "Unable to open file, or file is empty.";
 }
 
-bool validator(std::string xml) {
-    return 0;
+bool validator(std::string xmlIn) {
+    std::string xml = xmlIn;
+    std::stack<std::string> valStack;
+    std::string currentCheck;
+    while (xml.size() != 0) {
+        if(!valStack.empty()) std::cout << valStack.top() << std::endl;
+        //check the tag
+        //get substring between < and >
+        std::cout << "<: " << xml.find_first_of('<') << std::endl;
+        std::cout << ">: " << (xml.find_first_of('>') + 1) << std::endl;
+
+        currentCheck = xml.substr(xml.find_first_of('<'), (xml.find_first_of('>')+1)- (xml.find_first_of('<')));
+        std::cout << "currentCheck: " << currentCheck << std::endl;
+        //open tag?
+        if (currentCheck.at(1) != '/') {
+            valStack.push(currentCheck);
+        }
+        //add to stack
+    //closing tag?
+        else { 
+            std::cout << "Closing" << std::endl;
+            std::cout << valStack.top().substr(1, (valStack.top().find_first_of(">") - 1)) << std::endl;
+            std::cout << currentCheck.substr(2, (currentCheck.find_first_of(">")-2)) << std::endl;
+            if (valStack.top().substr(1, (valStack.top().find_first_of(">") - 1)) == currentCheck.substr(2, currentCheck.find_first_of(">")-2)) valStack.pop();
+            else return false;
+        };
+            //take away from stack, non-matching returns false
+        xml = xml.substr(xml.find_first_of(">")+1);
+    }
+    return valStack.empty();
 }
