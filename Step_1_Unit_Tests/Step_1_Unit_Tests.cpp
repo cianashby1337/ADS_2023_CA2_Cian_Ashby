6ccac77f0a05933e8d46d8926bebd33ba7c3a6d9
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2023-12-02 00:16:33 +0000

    Create improper nesting tests for validator method

--- Step_1_Unit_Tests/Step_1_Unit_Tests.cpp
+++ Step_1_Unit_Tests/Step_1_Unit_Tests.cpp
@@ -40,5 +40,20 @@ namespace Step1UnitTests
 			std::string expectedString = "<name>ADS_Single_LinkedList_Exercises</name><dir><name>.git</name><file><name>config</name><length>353b</length><type>config</type></file><file><name>description</name><length>73b</length><type>description</type></file><file><name>HEAD</name><length>23b</length><type>HEAD</type></file></dir><dir><name>.vs</name><dir><name>ADS_Single_LinkedList_Exercises</name><dir><name>v17</name></dir></dir></dir><dir><name>Debug</name><file><name>SinglyLinkedList.pdb</name><length>978944b</length><type>pdb</type></file><file><name>TestSinglyLinkedList.exp</name><length>41528b</length><type>exp</type></file><file><name>TestSinglyLinkedList.pdb</name><length>1945600b</length><type>pdb</type></file></dir>";
 			Assert::IsFalse(validator(expectedString));
 		}
+
+		TEST_METHOD(TestValidateFolderWithSize) {
+			std::string expectedString = "<dir><size>4b</size><name></name></dir>";
+			Assert::IsFalse(validator(expectedString));
+		}
+
+		TEST_METHOD(TestValidateFolderWithType) {
+			std::string expectedString = "<dir><type>config</type><name>ADS_Single_LinkedList_Exercises</name><dir><name>.git</name><file><name>config</name><length>353b</length><type>config</type></file><file><name>description</name><length>73b</length><type>description</type></file><file><name>HEAD</name><length>23b</length><type>HEAD</type></file></dir><dir><name>.vs</name><dir><name>ADS_Single_LinkedList_Exercises</name><dir><name>v17</name></dir></dir></dir><dir><name>Debug</name><file><name>SinglyLinkedList.pdb</name><length>978944b</length><type>pdb</type></file><file><name>TestSinglyLinkedList.exp</name><length>41528b</length><type>exp</type></file><file><name>TestSinglyLinkedList.pdb</name><length>1945600b</length><type>pdb</type></file></dir></dir>";
+			Assert::IsFalse(validator(expectedString));
+		}
+
+		TEST_METHOD(TestValidateFileWithDir) {
+			std::string expectedString = "<dir><type>config</type><name>ADS_Single_LinkedList_Exercises</name><dir><name>.git</name><file><name>config</name><length>353b</length><type>config</type></file><file><dir></dir><name>description</name><length>73b</length><type>description</type></file><file><name>HEAD</name><length>23b</length><type>HEAD</type></file></dir><dir><name>.vs</name><dir><name>ADS_Single_LinkedList_Exercises</name><dir><name>v17</name></dir></dir></dir><dir><name>Debug</name><file><name>SinglyLinkedList.pdb</name><length>978944b</length><type>pdb</type></file><file><name>TestSinglyLinkedList.exp</name><length>41528b</length><type>exp</type></file><file><name>TestSinglyLinkedList.pdb</name><length>1945600b</length><type>pdb</type></file></dir></dir>";
+			Assert::IsFalse(validator(expectedString));
+		}
 	};
 }
