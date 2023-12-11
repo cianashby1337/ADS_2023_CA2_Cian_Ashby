5ebdd9566872e7291c22a44351f5face7a60e83f
Cian Ashby <cianashby1337@gmail.com>
2023-12-11 14:51:22 +0000

    Implement console interface within main.cpp

--- ADS_CA2_Tree_Project/main.cpp
+++ ADS_CA2_Tree_Project/main.cpp
@@ -4,9 +4,57 @@
 #include "File.h"
 
 int main() {
-	std::string expectedString = "<dir><name>ADS_Single_LinkedList_Exercises</name><dir><name>.git</name><file><name>config</name><length>353b</length><type>config</type></file><file><name>description</name><length>73b</length><type>description</type></file><file><name>HEAD</name><length>23b</length><type>HEAD</type></file></dir><dir><name>.vs</name><dir><name>ADS_Single_LinkedList_Exercises</name><dir><name>v17</name></dir></dir></dir><dir><name>Debug</name><file><name>SinglyLinkedList.pdb</name><length>978944b</length><type>pdb</type></file><file><name>TestSinglyLinkedList.exp</name><length>41528b</length><type>exp</type></file><file><name>TestSinglyLinkedList.pdb</name><length>1945600b</length><type>pdb</type></file></dir></dir>";
-	Tree<File>* testTree = createFileSystem(expectedString);
+	std::string fileName = "../../vs_sample.xml";
+	std::string xmlContents = readFile(fileName);
+	Tree<File>* testTree = createFileSystem(xmlContents);
 	TreeIterator<File> treeIter(testTree);
-	std::cout << findSearchItem(treeIter, "BeansAndToast.grom") << std::endl;
+	int choiceInt;
+	bool running = true;
+	std::string choiceString;
+	while (running) {
+		std::cout << "\n" << findWorkingDirectory(treeIter.node) << std::endl;
+		std::cout << displayContents(treeIter.node) << std::endl;
+		std::cin >> choiceInt;
+		if (choiceInt == 0) {
+			std::cout << "\nChoose an action from below" << std::endl;
+			std::cout << "0. Move up a level" << std::endl;
+			std::cout << "1. Search for a file/folder" << std::endl;
+			std::cout << "2. Calculate memory usage" << std::endl;
+			std::cout << "3. Count files/folders in current directory" << std::endl;
+			std::cout << "4. Prune tree" << std::endl;
+			std::cout << "5. Exit Program" << std::endl;
+			std::cin >> choiceInt;
+
+			if (choiceInt == 0) {
+				if (treeIter.node->parent != nullptr) treeIter.up();
+				else std::cout << "You cannot move up past the root directory" << std::endl;
+			}
+			else if (choiceInt == 1) {
+				std::cout << "Enter the name of the file" << std::endl;
+				std::cin >> choiceString;
+				std::cout << findSearchItem(treeIter, choiceString) << std::endl;
+			}
+			else if (choiceInt == 2) 
+				std::cout << "Total memory usage within the current directory:" << sumDirectoryMemoryUsage(treeIter.node) << " b" << std::endl;
+			else if (choiceInt == 3) 
+				std::cout << "Number of entities in current directory tree:" << countWithinDirectory(treeIter.node, treeIter.node) << std::endl;
+			else if (choiceInt == 4) 
+				std::cout << "Under Construction" << std::endl;
+			else if (choiceInt == 5)
+				running = !running;
+			else 
+				std::cout << choiceInt << " is not a valid option, please try again" << std::endl;
+		}
+		else {
+			if (choiceInt - 1 >= treeIter.node->children->count) std::cout << "No" << std::endl;
+			else {
+				treeIter.childStart();
+				for (int i = 0; i < choiceInt-1; i++) treeIter.childIter.advance();
+
+				if (treeIter.childIter.currentNode->data->data.isDirectory) treeIter.down();
+				else std::cout << "You cannot navigate into a file" << std::endl;
+			}
+		}
+	}
 	return 0;
 }
