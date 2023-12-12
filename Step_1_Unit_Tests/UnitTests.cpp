94d970a36b160ec64874065662bbe34dc3c1d3b9
Cian Ashby <cianashby1337@gmail.com>
2023-12-12 09:45:52 +0000

    Create the unit test for Stage 2: c

--- Step_1_Unit_Tests/UnitTests.cpp
+++ Step_1_Unit_Tests/UnitTests.cpp
@@ -160,5 +160,15 @@ namespace Step1UnitTests
 			std::string output = findSearchItem(treeIter, "BeansAndToast.grom");
 			Assert::IsTrue("No match was found for BeansAndToast.grom" == output);
 		}
+
+		TEST_METHOD(TestPrune) {
+			std::string xml = "<dir><name>ADS_Single_LinkedList_Exercises</name><dir><name>.git</name><file><name>config</name><length>353b</length><type>config</type></file><file><name>description</name><length>73b</length><type>description</type></file><file><name>HEAD</name><length>23b</length><type>HEAD</type></file></dir><dir><name>.vs</name><dir><name>ADS_Single_LinkedList_Exercises</name><dir><name>v17</name></dir></dir></dir><dir><name>Debug</name><file><name>SinglyLinkedList.pdb</name><length>978944b</length><type>pdb</type></file><file><name>TestSinglyLinkedList.exp</name><length>41528b</length><type>exp</type></file><file><name>TestSinglyLinkedList.pdb</name><length>1945600b</length><type>pdb</type></file></dir></dir>";
+			Tree<File>* t = createFileSystem(xml);
+			TreeIterator<File> treeIter(t);
+			int countBefore = countWithinDirectory(t);
+			pruneTree(&treeIter);
+			int countAfter = countWithinDirectory(t);
+			Assert::AreNotEqual(countAfter, countBefore);
+		}
 	};
 }
